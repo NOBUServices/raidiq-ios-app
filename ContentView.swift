76fce2733d7c19e52fa10fdb43c119cefa50c4ed
NOBUServices <codenobu842@gmail.com>
2025-06-29 14:10:02 +0100

    Add files via upload

---
+++ ContentView.swift
@@ -0,0 +1,22 @@
+import SwiftUI
+import WebKit
+
+struct ContentView: View {
+    var body: some View {
+        WebView(url: URL(string: "https://raid-iq.com")!)
+            .edgesIgnoringSafeArea(.all)
+    }
+}
+
+struct WebView: UIViewRepresentable {
+    let url: URL
+
+    func makeUIView(context: Context) -> WKWebView {
+        return WKWebView()
+    }
+
+    func updateUIView(_ uiView: WKWebView, context: Context) {
+        let request = URLRequest(url: url)
+        uiView.load(request)
+    }
+}
